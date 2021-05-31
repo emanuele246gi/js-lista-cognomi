3a17b9006111370b4a597555db935a69185643f4
Emanuele <emanuele65b@gmail.com>
2021-05-31 17:14:43 +0200

    add controllo email

--- js/script.js
+++ js/script.js
@@ -1,2 +1,29 @@
 // Prompt email
 var email = prompt ('Inserisci qui la tua email');
+
+var listaemail = ["neri@gmail.com", "bianchi@gmail.com", "rossi@gmail.com", "verdi@gmail.com", "gialli@gmail.com"];
+
+for (var i = 0; i < 4; i++){
+    if (email == listaemail[i]){
+        var controlli = "convalidato";
+    }
+}
+
+if (controlli == "convalidato"){
+
+    alert("Benvenuto nel club!");
+
+} else {
+
+    alert("Tu chi sei?");
+
+}
+
+// // Prompt cognome
+// var cognome = prompt ('Inserisci qui il tuo cognome');
+
+// var listacognomi = ["Neri", "Bianchi", "Rossi", "Verdi", "Gialli"];
+
+
+
+// // controlli per l'accesso
